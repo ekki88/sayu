c409abdc76dce58e876df188ced935bfccb15c50
bomi <ekki88@naver.com>
2024-06-17 19:41:07 +0900

    test

---
+++ src/loading/Loading.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import img01 from 'src/img/01.jpg';
+// import img02 from '/src/img/02.jpg';
+// import img03 from '/src/img/03.jpg';
+// import img04 from '/src/img/04.png';
+// import four from 'src/four.jpg';
+
+const Loading = () => {
+    return (
+        <div>
+            <img src="/src/img/01.jpg" alt="logo"/>
+            <img src="/src/four.jpg" alt="logo"/>
+        </div>
+
+    );
+};
+
+export default Loading;
---
+++ src/loading/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            포스터
+        </div>
+    );
+};
+
+export default Main;
