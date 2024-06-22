d53eeaa6e4096f5611744ad1bb979eb3b6e0ee0c
bomi <ekki88@naver.com>
2024-06-23 00:38:12 +0900

    css

--- src/loading/Main.jsx
+++ src/loading/Main.jsx
@@ -2,12 +2,13 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import styled from "styled-components";
 import Pagination from 'react-js-pagination'
+import ReactPaginate from "react-paginate";
 
 
 const Main = () => {
     const [data, setData] = useState();
-    const [page, setPage] = useState(1);
-    const [items, setItem] = useState(5)
+    const [currentPage, setCurrentPage] = useState(0);
+    const itemsPerPage = 6;
     const api_key = process.env.REACT_APP_API_KEY;
     const today = new Date()
     const year = today.getFullYear();
@@ -20,35 +21,57 @@ const Main = () => {
             try{
                 const response = await axios.get(`http://openapi.seoul.go.kr:8088/${api_key}/json/culturalEventInfo/1/1000/전시/ `)
                 const result = response.data
-                console.log(result.culturalEventInfo.row);
-                setData(result);
+                const filteredData = result.culturalEventInfo.row.filter(
+                    item => item.GUNAME === "종로구" && item.STRTDATE <= time && item.END_DATE >= time
+                );
+                setData(filteredData);
             }
             catch(e) {
                 console.log(e)
             }
         }
         getData()
-    },[api_key])
+    },[])
+
+    const handlePageClick = (event) => {
+        setCurrentPage(event.selected);
+    };
+
+    const offset = currentPage * itemsPerPage;
+    const currentPageData = Array.isArray(data) ? data.slice(offset, offset + itemsPerPage) : [];
+    const onClick = () =>{
+        console.log('전시 상세페이지로 ')
+    }
+    const onClickPlace = () => {
+        console.log('장소 상세페이지 , 카카오 ')
+    }
     return (
         <Container>
             <div>
-                <b> SAYU </b>
-                <p>진행중인 행사</p>
+                <b> SAYU : 사대문 안에서 뭘 하지?  </b>
             </div>
             <List>
-                {data && data.culturalEventInfo.row.map((item)=>{
-                    if(item.GUNAME == "종로구" && item.STRTDATE < time && item.END_DATE > time ) {
-                        return(
-                            <Box>
-                                <Img src={item.MAIN_IMG} alt='poster' key={item.id}/>
-                                <p>{item.DATE}</p>
-                                <p>{item.TITLE}</p>
-                                <p>{item.PLACE}</p>
-                            </Box>
-                        )
-                    }
-                })}
+                {currentPageData.map((item, index) => (
+                    <Box key={index}>
+                        <Img src={item.MAIN_IMG} alt="poster" onClick={onClick}/>
+                        <Title onClick={onClick}>{item.TITLE}</Title>
+                        <Place onClick={onClickPlace}>{item.PLACE}</Place>
+                    </Box>
+                ))}
             </List>
+            <PaginationBox>
+                <ReactPaginate
+                    previousLabel={"이전"}
+                    nextLabel={"다음"}
+                    breakLabel={"..."}
+                    pageCount={Math.ceil( itemsPerPage)}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={handlePageClick}
+                    containerClassName={"pagination"}
+                    activeClassName={"active"}
+                />
+            </PaginationBox>
         </Container>
 
     );
@@ -59,22 +82,48 @@ export default Main;
 
 const Container = styled.div`
     display: flex;
+    flex-direction: column;
     justify-content: center;
     width: 100vw;
     height: 100%;
+    place-items: center;
 `
-const Img = styled.img`
-    width: 20vw;
-    height: 50vh;
-`
-
 const List = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr ;
-    width: 100%;
+    grid-template-columns: 0.5fr 0.5fr 0.5fr ;
+    width: 1000px;
     height: 100%;
 `
 
+const Img = styled.img`
+    width: 300px;
+    height: 450px;
+`
+const Title = styled.p`
+    font-weight: bold;
+    word-break: keep-all;
+`
+const Place = styled.p`
+    color: dimgrey;
+    word-break: keep-all;
+`
 const Box =styled.div`
     margin: 10px;
 `
+const PaginationBox = styled.div`
+    display: flex;
+    justify-content: center; 
+    margin-top: 15px;
+  ul { 
+      display: flex;
+      list-style: none; 
+      padding: 0; }
+  ul.pagination li {
+    width: 40px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+  }
+  `
